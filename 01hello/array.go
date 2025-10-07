b69bbfeffbe0140b0b68960974dbf167da1920e3
NIROOZbx <nyroozashraf@gmail.com>
2025-10-07 19:00:08 +0530

    find min max

--- 01hello/array.go
+++ 01hello/array.go
@@ -1,12 +1,27 @@
 package main
 import "fmt"
 
-func main(){
-	 cars:= []string{"bugatti","lamborghini","honda","toyota"}
-	 cars=append(cars,"Mustang")
+func findMinMax()(int,int){
+	var nums=[7]int{3,2,9,24,1,4,5}
 
-	for i:=0; i<len(cars); i++ {
-		fmt.Println(cars[i])
-		
+	min:=nums[0]
+	max:=nums[0]
+
+	for i:=0;i<len(nums);i++{
+		if min>nums[i]{
+			min=nums[i]
+		}
+		if max<nums[i]{
+			max=nums[i]
+		}
 	}
+
+	return max,min
+
+
+}
+
+func main(){
+	
+	 fmt.Println(findMinMax())
 }
